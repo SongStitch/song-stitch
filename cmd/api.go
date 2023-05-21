0b85d5d0dd114709157fc464cb3f652df36f7a70
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-05-20 22:17:23 -0400

    Adding basic structure for collage endpoint

--- cmd/api.go
+++ cmd/api.go
@@ -2,11 +2,37 @@ package main
 
 import (
 	"fmt"
+	"github.com/dyninc/qstring"
 	"net/http"
+	"net/url"
 )
 
-func collage(w http.ResponseWriter, r *http.Request) {
+type CollageRequest struct {
+	Width    int    `url:"width"`
+	Height   int    `url:"height"`
+	Username string `url:"username"`
+	Period   string `url:"period"`
+}
 
+func collage(w http.ResponseWriter, r *http.Request) {
+	queryParams, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var request CollageRequest
+
+	err = qstring.Unmarshal(queryParams, &request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Width: %d\n", request.Width)
+	fmt.Fprintf(w, "Height: %d\n", request.Height)
+	fmt.Fprintf(w, "Username: %s\n", request.Username)
+	fmt.Fprintf(w, "Period: %s\n", request.Period)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/SongStitch/song-stitch
 
 go 1.20
+
+require github.com/dyninc/qstring v0.0.0-20160719172318-ab5840a88e81 // indirect
