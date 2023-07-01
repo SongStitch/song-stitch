a28b3c015caa16c4a40f3f0f2c01e0202a0d4c59
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-07-01 10:30:23 -0400

    fix static assets

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    minify: true,
+    minify: false,
     outDir: '../public',
     emptyOutDir: true,
   },
