e2491d365a9fef9ff359cc6e747e8c478397689f
TheDen <deniskhoshaba@gmail.com>
2023-11-29 22:22:23 +1100

    add golines to CI + Makefile

--- internal/api/validators.go
+++ internal/api/validators.go
@@ -8,7 +8,12 @@ import (
 func validatePeriod(fl validator.FieldLevel) bool {
 	period := constants.Period(fl.Field().String())
 	switch period {
-	case constants.OVERALL, constants.SEVEN_DAYS, constants.ONE_MONTH, constants.THREE_MONTHS, constants.SIX_MONTHS, constants.TWELVE_MONTHS:
+	case constants.OVERALL,
+		constants.SEVEN_DAYS,
+		constants.ONE_MONTH,
+		constants.THREE_MONTHS,
+		constants.SIX_MONTHS,
+		constants.TWELVE_MONTHS:
 		return true
 	default:
 		return false
@@ -18,7 +23,12 @@ func validatePeriod(fl validator.FieldLevel) bool {
 func validateTextLocation(fl validator.FieldLevel) bool {
 	textLocation := constants.TextLocation(fl.Field().String())
 	switch textLocation {
-	case constants.TOP_LEFT, constants.TOP_CENTRE, constants.TOP_RIGHT, constants.BOTTOM_LEFT, constants.BOTTOM_CENTRE, constants.BOTTOM_RIGHT:
+	case constants.TOP_LEFT,
+		constants.TOP_CENTRE,
+		constants.TOP_RIGHT,
+		constants.BOTTOM_LEFT,
+		constants.BOTTOM_CENTRE,
+		constants.BOTTOM_RIGHT:
 		return true
 	default:
 		return false
